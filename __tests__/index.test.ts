520a13d54870958b0a56d7a039a2d8a00d41b10f
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-07-14 19:15:00 +0200

    1.3.1 Changed "dist" dir to "lib"

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import NodeGeolocation from '../dist/cjs/index.js';
+import NodeGeolocation from '../lib/cjs/index.js';
 
 const { IPINFO_KEY, TESTIP, IP2LOCATION_KEY } = process.env;
 
